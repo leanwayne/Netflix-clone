3a6201cbc35238ded485c0665cc92b855b3f78be
leanwayne <lean_rap50@hotmail.com>
2022-08-11 17:37:37 -0300

    react player fix

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,21 +1,31 @@
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { modalState, movieState } from '../atoms/modalAtom.'
-import MuiModal from "@mui/material/Modal"
-import { CheckIcon, PlusIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon, XIcon } from '@heroicons/react/solid'
 import { useEffect, useState } from 'react'
-import { Movie, Element, Genre } from '../typings'
-import ReactPlayer from 'react-player/'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom.'
 import { FaPlay } from 'react-icons/fa'
+import MuiModal from "@mui/material/Modal"
+import {
+    CheckIcon,
+    PlusIcon,
+    ThumbUpIcon,
+    VolumeOffIcon,
+    VolumeUpIcon,
+    XIcon,
+} from '@heroicons/react/outline'
+import { Movie, Element, Genre } from '../typings'
+import ReactPlayer from 'react-player/lazy'
+import { DocumentData } from 'firebase/firestore'
 
 const Modal = () => {
       const [showModal, setShowModal] = useRecoilState(modalState)
       const [movie, setMovie] = useRecoilState(movieState)
       const [trailer, setTrailer] = useState("")
       const [genres, setGenres] = useState<Genre[]>([])
-      const [muted, setMuted] = useState(false)
+      const [muted, setMuted] = useState(true)
+      const [movies, setMovies] = useState<DocumentData[] | Movie[]>([])
 
     useEffect(() => {
         if(!movie) return
+
         async function fetchMovie() {
             const data = await fetch(`https://api.themoviedb.org/3/${movie?.media_type === 'tv' ? 'tv' : 'movie'}/${movie?.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`)
             .then((response) => response.json())
@@ -29,11 +39,13 @@ const Modal = () => {
                 setGenres(data.genres)
             }
         }
+
         fetchMovie()
     },[movie])
 
     const handleClose = () => {
-        setShowModal(false)  
+        setShowModal(false)
+        
     }
 
     return (
@@ -43,7 +55,7 @@ const Modal = () => {
                     <XIcon className='h-6 w-6'/>
                 </button>
 
-                <div className='relative -pt-[56.25%]'>
+                <div className="relative pt-[56.25%]">
                     <ReactPlayer
                         url={`https://www.youtube.com/watch?v=${trailer}`}
                         width="100%"
@@ -52,6 +64,7 @@ const Modal = () => {
                         playing
                         muted={muted}
                     />
+
                     <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
                         <div className="flex space-x-2">
                             <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
@@ -59,9 +72,7 @@ const Modal = () => {
                                 Play
                             </button>
                             <button className="modalButton">
-                  
                                 <PlusIcon className="h-7 w-7" />
-                               
                             </button>
                             <button className="modalButton">
                                 <ThumbUpIcon className="h-6 w-6" />
@@ -76,7 +87,6 @@ const Modal = () => {
                         </button>
                     </div>
                 </div>
-
                 <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
                     <div className="space-y-6 text-lg">
                         <div className="flex items-center space-x-2 text-sm">
